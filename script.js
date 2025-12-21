89a3aa262362871a28e41d14ee44de5c540d85f7
Melvin <espegaderamelvin@gmail.com>
2025-12-21 19:30:08 +0800

    Add PWA: manifest, service worker, icons for app

--- script.js
+++ script.js
@@ -51,3 +51,12 @@ function toggleMenu() {
       icon.src = icon.getAttribute("src-light");
     });
   }
+
+  if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(reg => console.log("Service Worker Registered", reg))
+      .catch(err => console.log("Service Worker Registration Failed", err));
+  });
+}
